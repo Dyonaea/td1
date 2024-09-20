eea3eb975074b77e1b55e1bbe794cbd75945e266
cherrie235 <mathias.cherrier@gmail.com>
2024-09-20 11:01:54 +0200

    shifumi was done perfectly without css

---
+++ js/helloworld.js
@@ -0,0 +1,86 @@
+/*const personne = {
+    nom: "c",
+    age: "moi",
+    profession: "wsh",
+    ville: "quoicouhville"
+};
+personnes = [
+    {prenom: "John", nom: "Doe", age: 25, ville: "Rouen"},
+    {prenom: "Jane", nom: "Doe", age: 30, ville: "Paris"},
+    {prenom: "Jim", nom: "Doe", age: 35, ville: "Caen"}
+]
+personnes.push({prenom: " Marc", nom: "Doe", age: 32, ville: "Marseille"})
+personnes[0].prenom = "feur"
+personnes.splice(1,1)
+
+console.table(personnes);
+
+console.log(personne.nom);
+console.log(personne.age);
+console.log(personne.profession);
+console.log(personne.ville);*/
+
+//console.log(((4>=6)||("herbe" != "verte")) && !(((12*2) == 144) && true))
+/*
+let poids = 59.0
+let taille = 1.81
+
+let imc = poids/(taille*taille)
+imc = imc.toFixed(2)
+console.log(imc , "faut manger wsh")
+
+const prixMin = 1000
+
+function lesSoussous(prix){
+    console.log(prix > prixMin ? "gratouit" : "pasgratuit")
+}
+
+lesSoussous(100)
+
+let title = document.getElementById('title')
+for(let i = 0; i < 10; i++){
+    title.textContent += "Booh"
+}
+
+let binaryNb = "0"
+let parse = parseInt(binaryNb)
+console.log(binaryNb)
+console.log(parse)
+*/
+
+/*
+let numbers = []
+for(let i = 0; i < 10; i++){
+    numbers[i] = i+1
+}
+console.log(numbers[0])
+console.log(numbers[numbers.length-1])
+console.log(numbers.length)
+
+let numbersCent = []
+for(let i = 0; i < 10; i++){
+    numbersCent[i] = (i+1)*10
+}
+console.table(numbersCent)
+console.table(numbersCent.reverse())
+*/
+
+/*
+numbers = [ 2, 5, 1, 9, 0, 3, 7, 4, 6, 8 ]
+
+console.table(numbers)
+console.table(numbers.sort())
+
+numbers.push(11)
+console.table(numbers)
+
+numbers.unshift(-1)
+console.table(numbers)
+
+numbers.pop()
+console.table(numbers)
+*/
+
+
+
+
---
+++ js/shifumi.js
@@ -0,0 +1,59 @@
+let papier = document.getElementById("papier")
+let caillou = document.getElementById("caillou")
+let decoupage = document.getElementById("decoupage")
+let resj = document.getElementById("resj")
+let resb = document.getElementById("resb")
+let res = document.getElementById("res")
+
+papier.addEventListener('click', () => {
+    jouer("papier")
+})
+
+caillou.addEventListener('click', () => {
+    jouer("pierre")
+})
+
+decoupage.addEventListener('click', () => {
+    jouer("ciseaux")
+})
+
+function jouer(rep){
+    let bot = Math.floor(Math.random() *3)
+    
+    let botfpc
+    switch(bot){
+        case 0:
+            botfpc = "pierre"
+            break;
+        case 1:
+            botfpc = "papier"
+            break;
+        case 2: 
+            botfpc = "ciseaux"           
+            break;
+        default:
+            console.log("la mort", bot)
+            break;
+    }
+    console.log("you:", rep)
+    console.log("bot:", botfpc)
+    console.log(whoWins(rep, botfpc))
+
+}
+
+function whoWins(j1, j2){
+    resj.textContent = j1
+    resb.textContent = j2
+
+    if(j1 == j2) res.textContent = "egalite" 
+    else{
+        if((j1 == "pierre" && j2 == "ciseaux") ||
+            (j1 == "ciseaux" && j2 == "papier") || 
+            (j1 == "papier" && j2 == "pierre")) {
+                res.textContent = "you win"
+            }
+        else{
+            res.textContent = "you lose" 
+        }
+    }
+}
